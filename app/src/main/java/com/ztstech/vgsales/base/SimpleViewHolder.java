2afb9cd3aaca793e6caa9f6996b818c0058154e4
zhiyuan <zhiyuannice@icloud.com>
2017-08-07 23:25:49 +0800

    增加首页新闻界面

---
+++ app/src/main/java/com/ztstech/vgsales/base/SimpleViewHolder.java
@@ -0,0 +1,58 @@
+package com.ztstech.vgsales.base;
+
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by zhiyuan on 2017/7/27.
+ */
+
+public abstract class SimpleViewHolder<T> extends RecyclerView.ViewHolder{
+
+    private ItemViewClickListener itemViewClickListener;
+    private SimpleRecyclerAdapter<T> adapter;
+
+    public SimpleViewHolder(View itemView) {
+        this(itemView, null);
+    }
+
+    /**
+     * 传入adapter 设置onItemClickListener才会有效
+     * @param itemView
+     * @param adapter
+     */
+    public SimpleViewHolder(View itemView, @Nullable SimpleRecyclerAdapter<T> adapter) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+        if (adapter != null) {
+            itemViewClickListener = new ItemViewClickListener();
+            itemView.setOnClickListener(itemViewClickListener);
+            this.adapter = adapter;
+        }
+    }
+
+    public final void refresh(T data, int position) {
+        if (itemViewClickListener != null) {
+            itemViewClickListener.data = data;
+            itemViewClickListener.position = position;
+        }
+        refreshView(data);
+    }
+
+    protected abstract void refreshView(T data);
+
+    private class ItemViewClickListener implements View.OnClickListener {
+
+        T data;
+
+        int position;
+
+        @Override
+        public void onClick(View v) {
+            adapter.onItemClick(data, position);
+        }
+    }
+}
