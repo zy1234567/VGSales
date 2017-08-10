5e48b51bca6b4da2eea3e7f5894301e669bf6c2d
zhiyuan <zhiyuannice@icloud.com>
2017-08-11 00:09:29 +0800

    增加登录

---
+++ app/src/main/java/com/ztstech/mate/activitys/complete_info/FillInfoActivity.java
@@ -0,0 +1,16 @@
+package com.ztstech.mate.activitys.complete_info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ztstech.mate.R;
+
+
+public class FillInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_info);
+    }
+}
--- app/src/main/java/com/ztstech/mate/activitys/splash/SplashActivity.java
+++ app/src/main/java/com/ztstech/mate/activitys/splash/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 
 import com.ztstech.mate.R;
+import com.ztstech.mate.activitys.login.LoginActivity;
 import com.ztstech.mate.activitys.main.MainActivity;
 import com.ztstech.mate.base.BaseActivity;
 
@@ -16,7 +17,11 @@ public class SplashActivity extends BaseActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                toMain();
+                if (isUserLogin()) {
+                    toMain();
+                }else {
+                    toLogin();
+                }
             }
         }, 2000);
     }
@@ -33,5 +38,14 @@ public class SplashActivity extends BaseActivity {
     private void toMain() {
         startActivity(new Intent(this, MainActivity.class));
         finish();
+    }
+
+    private void toLogin() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+    private boolean isUserLogin() {
+        return false;
     }
 }
