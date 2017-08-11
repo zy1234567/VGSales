18d43ed7afc0f183e23542cd1d0dbe07605862fc
zhiyuan <zhiyuannice@icloud.com>
2017-08-11 21:54:43 +0800

    增加完成资料界面

--- app/src/main/java/com/ztstech/vgmate/activitys/complete_info/FillInfoActivity.java
+++ app/src/main/java/com/ztstech/vgmate/activitys/complete_info/FillInfoActivity.java
@@ -1,9 +1,12 @@
 package com.ztstech.vgmate.activitys.complete_info;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ztstech.vgmate.R;
+import com.ztstech.vgmate.activitys.main.MainActivity;
 
 
 public class FillInfoActivity extends AppCompatActivity {
@@ -12,5 +15,14 @@ public class FillInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_info);
+    }
+
+    /**
+     * 点击提交
+     * @param view
+     */
+    public void onSubmitClick(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
