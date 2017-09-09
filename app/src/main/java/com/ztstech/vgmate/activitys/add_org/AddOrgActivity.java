35ab4fd5ca020be8e3fb9d004aa9469acee02318
zhiyuan <zhiyuannice@icloud.com>
2017-09-09 17:28:49 +0800

    增加添加机构界面

---
+++ app/src/main/java/com/ztstech/vgmate/activitys/add_org/AddOrgActivity.java
@@ -0,0 +1,18 @@
+package com.ztstech.vgmate.activitys.add_org;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ztstech.vgmate.R;
+
+/**
+ * 增加机构
+ */
+public class AddOrgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_org);
+    }
+}
