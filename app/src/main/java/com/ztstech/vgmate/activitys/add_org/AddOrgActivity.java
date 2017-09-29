2b32fbb93159ef36651bfb1f6dca01b4e31396b2
zhiyuan <zhiyuannice@icloud.com>
2017-09-29 20:36:44 +0800

    调整添加机构界面

--- app/src/main/java/com/ztstech/vgmate/activitys/add_org/AddOrgActivity.java
+++ app/src/main/java/com/ztstech/vgmate/activitys/add_org/AddOrgActivity.java
@@ -1,15 +1,23 @@
 package com.ztstech.vgmate.activitys.add_org;
 
+import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.tbruyelle.rxpermissions.RxPermissions;
 import com.ztstech.vgmate.R;
 import com.ztstech.vgmate.activitys.MVPActivity;
+import com.ztstech.vgmate.activitys.add_sell_mate.AddSellMateActivity;
 import com.ztstech.vgmate.activitys.category_info.CategoryTagsActivity;
 import com.ztstech.vgmate.activitys.category_info.CategoryTagsPresenter;
 import com.ztstech.vgmate.activitys.gps.GpsActivity;
@@ -17,6 +25,7 @@ import com.ztstech.vgmate.activitys.location_select.LocationSelectActivity;
 import com.ztstech.vgmate.weigets.TopBar;
 
 import butterknife.BindView;
+import rx.functions.Action1;
 
 /**
  * 增加机构
@@ -30,6 +39,8 @@ public class AddOrgActivity extends MVPActivity<AddOrgContract.Presenter> implem
     public static final int REQ_GPS = 2;
     /**请求地址*/
     public static final int REQ_LOCATION = 3;
+    /**请求联系人*/
+    public static final int REQ_CONTACT = 4;
 
     @BindView(R.id.et_org_name)
     EditText etName;
@@ -37,12 +48,16 @@ public class AddOrgActivity extends MVPActivity<AddOrgContract.Presenter> implem
     EditText etDetailLocation;
     @BindView(R.id.et_desc)
     EditText etDesc;
+    @BindView(R.id.et_phone)
+    EditText etPhone;
     @BindView(R.id.tv_location)
     TextView tvLocation;
     @BindView(R.id.tv_tag)
     TextView tvTag;
     @BindView(R.id.tv_gps)
     TextView tvGps;
+    @BindView(R.id.tv_contact)
+    TextView tvContact;
 
     @BindView(R.id.top_bar)
     TopBar topBar;
@@ -63,6 +78,7 @@ public class AddOrgActivity extends MVPActivity<AddOrgContract.Presenter> implem
         tvLocation.setOnClickListener(this);
         tvGps.setOnClickListener(this);
         tvTag.setOnClickListener(this);
+        tvContact.setOnClickListener(this);
 
         topBar.getRightTextView().setOnClickListener(this);
     }
@@ -76,9 +92,43 @@ public class AddOrgActivity extends MVPActivity<AddOrgContract.Presenter> implem
         if (REQ_TAG == requestCode) {
             String name = data.getStringExtra(CategoryTagsPresenter.PARAM_NAME);
             String ids = data.getStringExtra(CategoryTagsPresenter.PARAM_ID);
-            // TODO: 2017/9/27 缓存id
             tvTag.setText(name);
             tvTag.setTag(ids);
+        }else if (REQ_GPS == requestCode) {
+            //获取经纬度
+            double la = data.getDoubleExtra(GpsActivity.RESULT_LATITUDE, 0);
+            double lo = data.getDoubleExtra(GpsActivity.RESULT_LONGITUDE, 0);
+            String location = data.getStringExtra(GpsActivity.RESULT_LOCATION);
+
+            tvGps.setText(la + "," + lo);
+            etDetailLocation.setText(location);
+        }else if (REQ_LOCATION == requestCode) {
+            //请求地址
+            String locationName = data.getStringExtra(LocationSelectActivity.RESULT_NAME);
+            String locationId = data.getStringExtra(LocationSelectActivity.RESULT_CODE);
+
+            tvLocation.setText(locationName);
+            tvLocation.setTag(locationId);
+        }else if (REQ_CONTACT == requestCode) {
+            //得到联系人
+            Uri contract = data.getData();
+            Cursor c = this.getContentResolver().query(contract, null, null, null, null);
+            if (c != null && c.moveToFirst()) {
+                String id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+
+                Cursor c2 = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + id, null, null);
+                if (c2 != null) {
+                    c2.moveToFirst();
+                    String phonenum = c2.getString(c2.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    etPhone.setText(phonenum);
+                    c2.close();
+                }
+
+            }
+            if (c != null) {
+                c.close();
+            }
         }
     }
 
@@ -99,6 +149,24 @@ public class AddOrgActivity extends MVPActivity<AddOrgContract.Presenter> implem
         }else if (view == topBar.getRightTextView()) {
             //保存
             submit();
+        }else if (view == tvContact) {
+            //通讯录
+            RxPermissions rxPermissions = new RxPermissions(this);
+            rxPermissions
+                    .request(Manifest.permission.READ_CONTACTS)
+                    .subscribe(new Action1<Boolean>() {
+                        @Override
+                        public void call(Boolean aBoolean) {
+
+                            if (aBoolean) {
+                                Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                                startActivityForResult(i , REQ_CONTACT);
+                            }else {
+                                Toast.makeText(AddOrgActivity.this, "无权限", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
         }
     }
 
--- app/src/main/java/com/ztstech/vgmate/activitys/gps/GpsActivity.java
+++ app/src/main/java/com/ztstech/vgmate/activitys/gps/GpsActivity.java
@@ -1,13 +1,17 @@
 package com.ztstech.vgmate.activitys.gps;
 
+import android.Manifest;
+import android.content.Intent;
 import android.location.Location;
 import android.os.PersistableBundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.maps.AMap;
@@ -24,20 +28,38 @@ import com.amap.api.services.geocoder.GeocodeSearch;
 import com.amap.api.services.geocoder.RegeocodeQuery;
 import com.amap.api.services.geocoder.RegeocodeResult;
 import com.kaopiz.kprogresshud.KProgressHUD;
+import com.tbruyelle.rxpermissions.RxPermissions;
 import com.ztstech.vgmate.R;
+import com.ztstech.vgmate.activitys.add_org.AddOrgActivity;
 import com.ztstech.vgmate.base.BaseActivity;
+import com.ztstech.vgmate.weigets.TopBar;
 
 import butterknife.BindView;
+import rx.functions.Action1;
 
 /**
  * 选择gps
  */
 public class GpsActivity extends BaseActivity implements  GeocodeSearch.OnGeocodeSearchListener {
 
+    /**延时*/
     public static final int DELAY = 200;
+
+    /**纬度*/
+    public static final String RESULT_LATITUDE = "result_latitude";
+    /**经度*/
+    public static final String RESULT_LONGITUDE = "result_longitude";
+    /**地址*/
+    public static final String RESULT_LOCATION = "result_location";
+
+
+
+
 
     @BindView(R.id.map)
     MapView mapView;
+    @BindView(R.id.top_bar)
+    TopBar topBar;
 
     private AMap aMap;
     private MyLocationStyle myLocationStyle;
@@ -50,6 +72,8 @@ public class GpsActivity extends BaseActivity implements  GeocodeSearch.OnGeocod
     private double la;
     /**精度*/
     private double lo;
+    /**选中的地址*/
+    private String location;
 
 
     private Runnable getLocationRunnable = new Runnable() {
@@ -61,7 +85,6 @@ public class GpsActivity extends BaseActivity implements  GeocodeSearch.OnGeocod
             RegeocodeQuery query = new RegeocodeQuery(new LatLonPoint(latLng.latitude, latLng.longitude),
                     100,GeocodeSearch.GPS);
 
-            marker.setPosition(latLng);
             la = latLng.latitude;
             lo = latLng.longitude;
             geocoderSearch.getFromLocationAsyn(query);
@@ -75,32 +98,104 @@ public class GpsActivity extends BaseActivity implements  GeocodeSearch.OnGeocod
     }
 
     @Override
-    protected void onViewBindFinish(@Nullable Bundle savedInstanceState) {
+    protected void onViewBindFinish(final @Nullable Bundle savedInstanceState) {
         super.onViewBindFinish(savedInstanceState);
         mapView.onCreate(savedInstanceState);
 
         progressHUD = new KProgressHUD(this);
 
+        //请求权限
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions
+                .request(Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean aBoolean) {
+                        if (aBoolean) {
+                            initMap(savedInstanceState);
+                        }else {
+                            Toast.makeText(GpsActivity.this, "无权限", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mapView != null) {
+            mapView.onDestroy();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        if (mapView != null) {
+            mapView.onSaveInstanceState(outState);
+        }
+    }
+
+    @Override
+    public void onRegeocodeSearched(final RegeocodeResult regeocodeResult, int i) {
+        if (isFinishing()) {
+            return;
+        }
+        if (regeocodeResult != null) {
+            progressHUD.dismiss();
+            LatLng latLng = new LatLng(la, lo);
+            if (regeocodeResult.getRegeocodeQuery().getPoint().getLatitude() == la &&
+                    regeocodeResult.getRegeocodeQuery().getPoint().getLongitude() == lo) {
+                location = regeocodeResult.getRegeocodeAddress().getFormatAddress();
+                if (marker == null) {
+                    marker = aMap.addMarker(new MarkerOptions().position(latLng)
+                            .title(regeocodeResult.getRegeocodeAddress().getFormatAddress()));
+                    marker.setPosition(latLng);
+                    marker.showInfoWindow();
+                    marker.setPositionByPixels(
+                            GpsActivity.this.getResources().getDisplayMetrics().widthPixels / 2,
+                            GpsActivity.this.getResources().getDisplayMetrics().heightPixels / 2);
+                }else {
+                    // FIXME: 2017/9/29 傻逼高德地图marker上textview只变大不变小
+                    marker.setTitle(regeocodeResult.getRegeocodeAddress().getFormatAddress());
+                    marker.showInfoWindow();
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+    }
+
+
+    private void initMap(Bundle savedInstanceState) {
         if (aMap == null) {
             aMap = mapView.getMap();
         }
 
         geocoderSearch = new GeocodeSearch(this);
         geocoderSearch.setOnGeocodeSearchListener(this);
-//        aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
-//            @Override
-//            public void onMyLocationChange(Location location) {
-//                if (marker == null) {
-//                    LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-//                    marker = aMap.addMarker(new MarkerOptions().position(latLng)
-//                                    .title(location.toString()).snippet("DefaultMarker"));
-//                }else {
-//
-//                }
-//
-//                marker.showInfoWindow();
-//            }
-//        });
+
 
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
@@ -110,22 +205,6 @@ public class GpsActivity extends BaseActivity implements  GeocodeSearch.OnGeocod
         aMap.getUiSettings().setMyLocationButtonEnabled(true); //设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
 
-        aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
-            @Override
-            public void onMyLocationChange(Location location) {
-                LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-                marker = aMap.addMarker(new MarkerOptions().position(latLng)
-                        .title(((com.amap.api.mapcore.util.hp)location).getAddress())
-                        .draggable(true));
-                marker.showInfoWindow();
-                marker.setPositionByPixels(
-                        GpsActivity.this.getResources().getDisplayMetrics().widthPixels / 2,
-                        GpsActivity.this.getResources().getDisplayMetrics().heightPixels / 2);
-                marker.setDraggable(true);
-                la = location.getLatitude();
-                lo = location.getLongitude();
-            }
-        });
 
 
         aMap.setAMapGestureListener(new AMapGestureListener() {
@@ -170,70 +249,26 @@ public class GpsActivity extends BaseActivity implements  GeocodeSearch.OnGeocod
                     marker.setTitle("正在定位");
                 }
                 mapView.postDelayed(getLocationRunnable, DELAY);
-
+                progressHUD.show();
             }
         });
 
 
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mapView != null) {
-            mapView.onDestroy();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mapView != null) {
-            mapView.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mapView != null) {
-            mapView.onResume();
-        }
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        if (mapView != null) {
-            mapView.onSaveInstanceState(outState);
-        }
-    }
-
-    @Override
-    public void onRegeocodeSearched(final RegeocodeResult regeocodeResult, int i) {
-        if (marker != null && regeocodeResult != null) {
-            if (regeocodeResult.getRegeocodeQuery().getPoint().getLatitude() == la &&
-                    regeocodeResult.getRegeocodeQuery().getPoint().getLongitude() == lo) {
-//                // FIXME: 2017/9/29 不能立即刷新，傻逼高德地图
-//                marker.setTitle(regeocodeResult.getRegeocodeAddress().getFormatAddress());
-//                mapView.invalidate();
-                if (marker != null) {
-                    marker.remove();
+        topBar.getRightTextView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (la == 0 && lo == 0) {
+                    setResult(RESULT_CANCELED);
+                }else {
+                    Intent it = new Intent();
+                    it.putExtra(RESULT_LATITUDE, la);
+                    it.putExtra(RESULT_LONGITUDE, lo);
+                    it.putExtra(RESULT_LOCATION, location);
+                    setResult(RESULT_OK, it);
                 }
-
-                marker = aMap.addMarker(new MarkerOptions().position(aMap.getCameraPosition().target)
-                        .title(regeocodeResult.getRegeocodeAddress().getFormatAddress())
-                        .draggable(true));
-                marker.showInfoWindow();
-                marker.setPositionByPixels(
-                        GpsActivity.this.getResources().getDisplayMetrics().widthPixels / 2,
-                        GpsActivity.this.getResources().getDisplayMetrics().heightPixels / 2);
+                onBackPressed();
             }
-        }
-    }
-
-    @Override
-    public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+        });
 
     }
 }
