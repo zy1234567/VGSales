935c1eff9d02173d5ee08a8671297b36346e87f6
zhiyuan <zhiyuannice@icloud.com>
2017-10-12 23:08:09 +0800

    调整评论窗口

--- app/src/main/java/com/ztstech/vgmate/activitys/article_detail/ArticleDetailActivity.java
+++ app/src/main/java/com/ztstech/vgmate/activitys/article_detail/ArticleDetailActivity.java
@@ -1,15 +1,28 @@
 package com.ztstech.vgmate.activitys.article_detail;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.text.Editable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.text.style.ImageSpan;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.ztstech.vgmate.R;
 import com.ztstech.vgmate.activitys.MVPActivity;
 import com.ztstech.vgmate.data.beans.MainListBean;
+import com.ztstech.vgmate.utils.ViewUtils;
 import com.ztstech.vgmate.weigets.TopBar;
 
 import butterknife.BindView;
@@ -18,7 +31,8 @@ import butterknife.BindView;
  * 文章详情界面
  */
 public class ArticleDetailActivity extends MVPActivity<ArticleDetailContract.Presenter> implements
-        ArticleDetailContract.View {
+        ArticleDetailContract.View, View.OnClickListener {
+
 
     /**是否显示编辑*/
     public static final String ARG_SHOW_EDIT = "arg_show_edit";
@@ -28,14 +42,23 @@ public class ArticleDetailActivity extends MVPActivity<ArticleDetailContract.Pre
 
     @BindView(R.id.web_view)
     WebView webView;
-
     @BindView(R.id.top_bar)
     TopBar topBar;
+    @BindView(R.id.et_comment)
+    EditText etComment;
+    @BindView(R.id.tv_submit)
+    TextView tvSubmit;
+    @BindView(R.id.tv_comment_count)
+    TextView tvCommentCount;
+    @BindView(R.id.img_comment)
+    ImageView imgComment;
 
     /**传入的数据*/
     private MainListBean.ListBean data;
     /**显示编辑*/
     private boolean showEdit;
+
+    private InputMethodManager imm;
 
     @Override
     protected int getLayoutRes() {
@@ -67,12 +90,116 @@ public class ArticleDetailActivity extends MVPActivity<ArticleDetailContract.Pre
             webView.loadUrl(data.url);
         }
 
+
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (etComment.isFocused()) {
+                    etComment.clearFocus();
+                }
+                return false;
+            }
+        });
+
+
+
+        imm = (InputMethodManager)
+                ArticleDetailActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        etComment.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    imm.showSoftInputFromInputMethod(etComment.getWindowToken(),0);
+
+                }else {
+                    imm.hideSoftInputFromWindow(etComment.getWindowToken() , 0);
+                }
+                setEditTextHint(b);
+                showCommentIconAndCount();
+            }
+        });
+
+        etComment.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                tvSubmit.setEnabled(editable.length() > 0);
+            }
+        });
+
+        imgComment.setOnClickListener(this);
+        tvSubmit.setOnClickListener(this);
+
+        //初始化状态
+        setEditTextHint(false);
+        showCommentIconAndCount();
     }
 
+    private void showCommentIconAndCount() {
+        if (etComment.isFocused()) {
+            tvCommentCount.setVisibility(View.INVISIBLE);
+            imgComment.setVisibility(View.INVISIBLE);
+            tvSubmit.setVisibility(View.VISIBLE);
+        }else {
+            if (data.evacnt > 0) {
+                tvCommentCount.setText(String.valueOf(data.evacnt));
+                tvCommentCount.setVisibility(View.VISIBLE);
+            }else {
+                tvCommentCount.setVisibility(View.INVISIBLE);
+            }
+            imgComment.setVisibility(View.VISIBLE);
+            tvSubmit.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    /**
+     * 设置edittext提示文字
+     * @param isEdit 是否处于编辑状态
+     */
+    private void setEditTextHint(boolean isEdit) {
+        if (isEdit) {
+            //如果获取到焦点
+            etComment.setHintTextColor(ContextCompat.getColor(ArticleDetailActivity.this,
+                    R.color.color_107));
+            etComment.setHint("写评论...");
+        }else {
+            SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder("   写评论...");
+            ImageSpan imageSpan = new ImageSpan(ArticleDetailActivity.this, R.mipmap.ico_com,
+                    ImageSpan.ALIGN_BASELINE);
+            spannableStringBuilder.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            etComment.setHintTextColor(ContextCompat.getColor(ArticleDetailActivity.this,
+                    R.color.color_100));
+            etComment.setHint(spannableStringBuilder);
+        }
+    }
+
+    /**
+     * 初始化数据
+     */
     private void initIntentData() {
         Intent it = getIntent();
         this.data = new Gson().fromJson(it.getStringExtra(ARG_LIST_DATA),
                 MainListBean.ListBean.class);
         this.showEdit = it.getBooleanExtra(ARG_SHOW_EDIT, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == imgComment) {
+            //跳转评论界面
+
+        }else if (view == tvSubmit) {
+            //提交评论
+
+        }
     }
 }
--- app/src/main/java/com/ztstech/vgmate/utils/ViewUtils.java
+++ app/src/main/java/com/ztstech/vgmate/utils/ViewUtils.java
@@ -6,6 +6,13 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.ImageSpan;
 import android.view.WindowManager;
 
 import com.ztstech.vgmate.base.BaseApplication;
@@ -57,6 +64,68 @@ public class ViewUtils {
         mWindowAttributes.width = point.x;
         mWindowAttributes.height = WindowManager.LayoutParams.MATCH_PARENT;
     }
+
+
+    /**
+     * 获取不同字号spannableStringBuilder
+     * @param builder 为空新建，不为空在现有基础上添加
+     * @param strs 分段字符串
+     * @param sizes 分段字符串大小，与分段字符串段数相同
+     * @return
+     */
+    public static SpannableStringBuilder getDiffSizeSpan(@Nullable SpannableStringBuilder builder,
+                                                         @NonNull String[] strs, @NonNull int[] sizes) {
+        String text = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            text = text + strs[i];
+        }
+        if (builder == null) {
+            builder = new SpannableStringBuilder(text);
+        }
+
+        int currentIndex = 0;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].isEmpty()) {
+                continue;
+            }
+            builder.setSpan(new AbsoluteSizeSpan(sizes[i]), currentIndex, currentIndex + strs[i].length(),
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            currentIndex += strs[i].length();
+        }
+        return builder;
+    }
+
+
+    /**
+     * 获取不同颜色span
+     * @param builder 为空新建，不为空在现有基础上添加
+     * @param strs 分段字符串
+     * @param colors 与分段字符串对应的颜色值
+     * @return
+     */
+    public static SpannableStringBuilder getDiffColorSpan(@Nullable SpannableStringBuilder builder,
+                                                          @NonNull String[] strs,@NonNull int[] colors) {
+        String text = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            text = text + strs[i];
+        }
+        if (builder == null) {
+            builder = new SpannableStringBuilder(text);
+        }
+
+        int currentIndex = 0;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].isEmpty()) {
+                continue;
+            }
+            builder.setSpan(new ForegroundColorSpan(colors[i]), currentIndex, currentIndex + strs[i].length(),
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            currentIndex += strs[i].length();
+        }
+        return builder;
+    }
+
+
 
 
 }
